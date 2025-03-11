a533c6cd11a10dcb4b914b4606e8eaf476c98425
alugand <alugand@gmail.com>
2025-03-11 14:17:58 +0100

    fix: setup bashio

--- ha_ecabinet/start.sh
+++ ha_ecabinet/start.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
+
+source /bashio/lib/bashio
+
 MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
-uvicorn main:app --host 0.0.0.0 --port 8080 &
+
+export MQTT_HOST MQTT_USER MQTT_PASSWORD
+
+
+uvicorn main:app --host 0.0.0.0 --port 8001 &
 python automations.py
