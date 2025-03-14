a6e7ec74f1e9a103ae9ea1c9075a6c0616093a25
alugand <alugand@gmail.com>
2025-03-14 10:23:31 +0100

    fix: update port

--- ecabinet_addon/run.sh
+++ ecabinet_addon/run.sh
@@ -7,5 +7,5 @@ MQTT_PASSWORD=$(bashio::services mqtt 'password')
 
 export MQTT_HOST MQTT_USER MQTT_PASSWORD
 
-uvicorn main:app --host 0.0.0.0 --port 8001 &
+uvicorn main:app --host 0.0.0.0 --port 8080 &
 python automations.py
